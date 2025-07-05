0aef97fceecbe3147c711e324f6b5ce4d368c4e9
YASH SUTHAR <yashsuthar0309@gmail.com>
2025-07-05 12:14:28 +0530

    URL redirect config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,44 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      // Redirect all variations to the main links.yashsuthar.com domain
+      {
+        "source": "/(.*)",
+        "destination": "https://links.yashsuthar.com/$1",
+        "permanent": true,
+        "has": [
+          {
+            "type": "host",
+            "value": "linktree.yashsuthar.com"
+          }
+        ]
+      },
+      {
+        "source": "/(.*)",
+        "destination": "https://links.yashsuthar.com/$1",
+        "permanent": true,
+        "has": [
+          {
+            "type": "host",
+            "value": "link.yashsuthar.com"
+          }
+        ]
+      },
+      {
+        "source": "/(.*)",
+        "destination": "https://links.yashsuthar.com/$1",
+        "permanent": true,
+        "has": [
+          {
+            "type": "host",
+            "value": "info.yashsuthar.com"
+          }
+        ]
+      },
+    ];
+  },
 };
 
 export default nextConfig;
